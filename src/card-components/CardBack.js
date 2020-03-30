7a67bb944aaedd0aeaedd3e90fd2fed071553339
rixong <rixong@gmail.com>
2020-03-30 17:56:36 -0500

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -10,19 +10,19 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 
 export default class CardBack extends Component {
 
-  // generateRatingElement = () => {
-  //   if (this.props.IMBDRating !== null) {
-  //     return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
-  //   }
-	// 	return <h4>No Rating Found</h4>;
-  // };
-
   generateRatingElement = () => {
-		if (this.props.IMDBRating !== null) {
-			return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
-		}
+    if (this.props.IMBDRating !== null) {
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
+    }
 		return <h4>No Rating Found</h4>;
-	};
+  };
+
+  // generateRatingElement = () => {
+	// 	if (this.props.IMDBRating !== null) {
+	// 		return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
+	// 	}
+	// 	return <h4>No Rating Found</h4>;
+	// };
 
   render() {
     return (
